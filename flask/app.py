7c4ea2e490f90bfa0af9bd27d0012058f880a12a
BEN AMOR Aziz <benamoraziz0220@gmail.com>
2023-08-07 16:54:47 +0100

    Latest HTML

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request, redirect, session, url_for
+from flask import Flask, render_template, Response, request, redirect, session, url_for, jsonify
 import face_recognition
 import cv2
 from flask_cors import CORS
@@ -21,7 +21,6 @@ db = pymysql.connect(
     port=3306
 )
 
-
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes in the app
 
@@ -29,10 +28,12 @@ CORS(app)  # Enable CORS for all routes in the app
 cap = None
 current_frame = None
 embeddings_unknown = np.empty((1, 128))
-placeholder_image = cv2.imread('/app/flask/static/placeholder.jpg')
+placeholder_image = cv2.imread('/app/static/placeholder.jpg')
 ret, buffer = cv2.imencode('.jpg', placeholder_image)
 no_frame = buffer.tobytes()
 saved_frame = no_frame
+
+
 
 def capture_frame():
     global cap, current_frame
@@ -41,6 +42,8 @@ def capture_frame():
             ret, frame = cap.read()
             if ret:
                 current_frame = frame
+                
+                
 
 def generate_frames():
     global current_frame
@@ -57,9 +60,15 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
+
+
 @app.route('/')
 def index():
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
     return render_template('index.html')
+
 
 
 @app.route('/save_shot')
@@ -112,7 +121,7 @@ def analyse_shot():
     if current_frame is not None:
         label = test(
             image=current_frame,
-            model_dir='resources/anti_spoof_models',
+            model_dir='/app/resources/anti_spoof_models',
             device_id=0
         )
         if label == 1:
@@ -145,6 +154,8 @@ def login():
         return 'Invalid username or password'
 
 
+# ...
+
 @app.route('/auth')
 def auth():
     global embeddings_unknown
@@ -156,8 +167,14 @@ def auth():
     match = False
     for user_image in user_images:
         face_image_binary = user_image[0]  # Assuming the face image is stored as a binary in the database
-        # Decode the face image from the database
-        face_image = face_recognition.load_image_file(io.BytesIO(face_image_binary))
+        # Attempt to load the face image from the binary data
+        try:
+            face_image = face_recognition.load_image_file(io.BytesIO(face_image_binary))
+        except OSError as e:
+            # Print the error message and continue to the next image
+            print(f"Error loading image: {e}")
+            continue
+
         # Encode the face from the database image
         face_encoding = face_recognition.face_encodings(face_image)
         if len(face_encoding) > 0:
@@ -165,15 +182,24 @@ def auth():
             # Compare the face encodings
             match = face_recognition.compare_faces([face_encoding], submitted_encoding)[0]
             if match:
-                break
+                    sql = "SELECT username FROM users WHERE face_image = %s"
+                    values = (user_image,)
+                    cursor.execute(sql, values)
+                    user = cursor.fetchone()
+                    session['username'] = user[0]
+                    break
+
     if match:
         return redirect(url_for('dashboard'))
     else:
         return 'unknown_person'
 
 
+
 @app.route('/dashboard')
 def dashboard():
+    if 'username' not in session:
+       return redirect(url_for('index'))
     username = session.get('username')
     cursor = db.cursor()
     sql = "SELECT face_image FROM users WHERE username = %s"
@@ -193,9 +219,24 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+
+@app.route('/is_face_detected')
+def isFaceDetected():
+    global current_frame
+    if current_frame is None:
+        return jsonify(False)
+    
+    embeddings_unknown = face_recognition.face_encodings(current_frame)
+    return jsonify(len(embeddings_unknown) > 0)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-    cap = cv2.VideoCapture('http://192.168.1.7:8080/video')
+    cap = cv2.VideoCapture(0)
     capture_thread = threading.Thread(target=capture_frame)
     capture_thread.start()
-    app.secret_key = '202020'
-    app.run(port=2023)
+    app.run(host='0.0.0.0', port=8000)
